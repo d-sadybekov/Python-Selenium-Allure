fca269dce6a41f67b47c9c14a4c5713131fb5a02
d.sadybekov <67111046+d-sadybekov@users.noreply.github.com>
2023-04-19 14:57:00 +0500

    Add files via upload

---
+++ tests/test_first.py
@@ -0,0 +1,52 @@
+"""
+Smoke test
+"""
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def test_product_view_sku():
+    """
+    Test case WERT-1
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("start-maximized") # open Browser in maximized mode
+    chrome_options.add_argument("--disable-infobars") # disabling infobars
+    chrome_options.add_argument("--disable-extensions") # disabling extensions
+    chrome_options.add_argument("--disable-gpu") #  applicable to windows os only
+    chrome_options.add_argument("--disable-dev-shm-usage") # overcome limited resource problems
+    # chrome_options.add_argument("--headless")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+   
+   ### Исправил УРЛ
+
+    url = "https://testqastudio.me"
+    driver.get(url=url)
+### ИСправил селектор на селектор раздела Бестселеры
+    element = driver.find_element(by=By.CSS_SELECTOR, value="[class='tab-best_sellers ']")
+    element.click()
+### Заменил товар и его СКУ соответвенно
+    # ищем по селектору карточку "ЭЛЛЬБИ Подвесная лампа" и кликаем по нему,
+    # чтобы просмотреть детали
+    element = driver.find_element(by=By.CSS_SELECTOR, value='[class*="post-11103"]')
+    element.click()
+
+    sku = driver.find_element(By.CLASS_NAME, value="sku")
+
+    assert sku.text == 'BFB9ZOK211', "Unexpected sku"
+
+@pytest.mark.smoke
+def test_smoke(browser):
+    """
+    Test case SMK-1
+    """
+    url = "https://testqastudio.me"
+    browser.get(url=url)
+    
